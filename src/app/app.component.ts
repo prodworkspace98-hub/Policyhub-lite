930c0acd83dcaddea52d9499bca6dfd9c847363d
Michael Laubscher <prodworkspace98@gmail.com>
2026-04-22 13:08:38 +0200

    --[Setup]
    -Setup of general structure or skeleton.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ShellComponent } from './core/layout/shell/shell.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, ShellComponent],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
